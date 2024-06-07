0aa5d865f6b21f5175869b33c8846aa646fa926d
Karel Wintersky <wombat@fontanka.ru>
2024-06-07 14:16:36 +0300

    1.3.4
    * [*] новая капча для регистрации (клаудфларовская)

--- registration_ajax_callback.php
+++ registration_ajax_callback.php
@@ -1,17 +1,25 @@
 <?php
+
+use FluxSoft\Turnstile\Turnstile;
+
 $dictionary = @include __DIR__ . '/templates/language/russian.php';
 $config_global = @include __DIR__ . '/config/config.php';
 $config_local = @include __DIR__ . '/config/config.local.php';
 
+$captcha_config = [
+    'secret_key'    =>  '0x4AAAAAAAcFuwELQ6CEykG91oF07uMnwLQ',
+    'site_key'      =>  '0x4AAAAAAAcFu1JCgXcTOghj'
+];
+
 $responseCode = '0';
 // get POST data
-$RespData = json_decode(file_get_contents('php://input'), true);
+$post_data = json_decode(file_get_contents('php://input'), true);
 
-function CallAPI($method, $url, $data = false){
+function CallAPI($method, $url, $data = false)
+{
     $curl = curl_init();
 
-    switch ($method)
-    {
+    switch ($method) {
         case "POST":
             curl_setopt($curl, CURLOPT_POST, 1);
 
@@ -40,92 +48,109 @@ function CallAPI($method, $url, $data = false){
     return $result;
 }
 
-function check_captcha($sCaptchaHash){
-	$data = array(
-			"secret"  => "6LeQ0lAaAAAAAKpQIcDu4VvQBpmsql1GXs2UrcwX",
-			"response" => $sCaptchaHash
-			);
-	$cap_res = CallAPI('POST', 'https://www.google.com/recaptcha/api/siteverify',  $data);
-		$response = json_decode($cap_res);
+function check_captcha_google($sCaptchaHash) {
+    $data = array(
+        "secret" => "6LeQ0lAaAAAAAKpQIcDu4VvQBpmsql1GXs2UrcwX",
+        "response" => $sCaptchaHash
+    );
+    $cap_res = CallAPI('POST', 'https://www.google.com/recaptcha/api/siteverify', $data);
+    $response = json_decode($cap_res);
 
-	return $response -> success; // true / false
+    return $response->success; // true / false
 }
 
-function create_user($login, $email, $password){
+/**
+ * https://packagist.org/packages/fluxsoft/php-turnstile
+ *
+ * @param $sCaptchaHash
+ * @return true|void
+ */
+function check_captcha_turnstile()
+{
+    global $captcha_config;
+
+    $turnstile = new Turnstile($captcha_config['secret_key']);
+    $verifyResponse = $turnstile->verify($_POST['cf-turnstile-response'], $_SERVER['REMOTE_ADDR']);
+
+    if ($verifyResponse->success) {
+        return true;
+    } else {
+        if ($verifyResponse->hasErrors()) {
+            foreach ($verifyResponse->errorCodes as $errorCode) {
+                echo $errorCode . '\n';
+            }
+        } else {
+            // unknown reason of failure resolving of captcha
+        }
+        return false;
+    }
+}
+
+function create_user($login, $email, $password)
+{
     global $dictionary;
     global $config_global;
     global $config_local;
-	
-	//echo 'Creating new user...' , PHP_EOL;
-	$_USER_LOGIN = $login;//readline('Enter login name: ');
-	$_USER_EMAIL = $email;//readline('Enter email: ');
-	$_USER_PASS = $password;//readline('Enter pass: ');
-	//echo '--------------------', PHP_EOL;
 
-	if (false == ($_USER_LOGIN && $_USER_EMAIL && $_USER_PASS)) {
-			return 'err_empty';
-			//die;
-	}	
+    //echo 'Creating new user...' , PHP_EOL;
+    $_USER_LOGIN = $login;//readline('Enter login name: ');
+    $_USER_EMAIL = $email;//readline('Enter email: ');
+    $_USER_PASS = $password;//readline('Enter pass: ');
+    //echo '--------------------', PHP_EOL;
 
-	$table_user = str_replace('___db.table.prefix___', $config_local['db']['table']['prefix'], $config_global['db']['table']['user']);
-	$table_blog = str_replace('___db.table.prefix___', $config_local['db']['table']['prefix'], $config_global['db']['table']['blog']);
-	$db_config = $config_local['db']['params'];
+    if (false == ($_USER_LOGIN && $_USER_EMAIL && $_USER_PASS)) {
+        return 'err_empty';
+        //die;
+    }
 
-	$db_host = $db_config['host'] ?? 'localhost';
-	$db_name = $db_config['dbname'] ?? 'mysql';
-	$db_user = $db_config['user'] ?? 'root';
-	$db_pass = $db_config['pass'] ?? '';
-	$db_port = $db_config['port'] ?? 3306;
+    $table_user = str_replace('___db.table.prefix___', $config_local['db']['table']['prefix'], $config_global['db']['table']['user']);
+    $table_blog = str_replace('___db.table.prefix___', $config_local['db']['table']['prefix'], $config_global['db']['table']['blog']);
+    $db_config = $config_local['db']['params'];
 
-	$dsl = sprintf("mysql:host=%s;port=%s;dbname=%s", $db_host, $db_port, $db_name);
-	$dbh = new PDO($dsl, $db_user, $db_pass);
-	$dbh->exec("SET NAMES utf8 COLLATE utf8_general_ci");
+    $db_host = $db_config['host'] ?? 'localhost';
+    $db_name = $db_config['dbname'] ?? 'mysql';
+    $db_user = $db_config['user'] ?? 'root';
+    $db_pass = $db_config['pass'] ?? '';
+    $db_port = $db_config['port'] ?? 3306;
 
-	//echo 'Checking: is exists user with given email or login? ';
-	$found_users = checkUserExist($dbh, $table_user, $_USER_LOGIN, $_USER_EMAIL);
+    $dsl = sprintf("mysql:host=%s;port=%s;dbname=%s", $db_host, $db_port, $db_name);
+    $dbh = new PDO($dsl, $db_user, $db_pass);
+    $dbh->exec("SET NAMES utf8 COLLATE utf8_general_ci");
 
-	if (empty($found_users)) {
-			//echo 'Not exists!', PHP_EOL;
-			
-			$uid = addUser($dbh, $table_user, $_USER_LOGIN, $_USER_EMAIL, $_USER_PASS);
-			
-			addBlog($dbh, $table_blog, $uid, $_USER_LOGIN);
-			
-			//echo 'New user added.' , PHP_EOL;
-			return 'ok';
-	} else {
-			// echo 'Exists!!!' , PHP_EOL;
-			// echo '-------------------------------------------', PHP_EOL;
-			// foreach ($found_users as $user) {
-					// echo "{$user['user_id']} | {$user['user_login']} | {$user['user_mail']} | {$user['user_profile_name']} | ", PHP_EOL;
-			// }
-			// echo '-------------------------------------------', PHP_EOL;
-			// echo 'Создать такого пользователя нельзя!', PHP_EOL;
-			return 'err_login_mail';
-	}	
+    //echo 'Checking: is exists user with given email or login? ';
+    $found_users = checkUserExist($dbh, $table_user, $_USER_LOGIN, $_USER_EMAIL);
+
+    if (empty($found_users)) {
+        //echo 'Not exists!', PHP_EOL;
+
+        $uid = addUser($dbh, $table_user, $_USER_LOGIN, $_USER_EMAIL, $_USER_PASS);
+
+        addBlog($dbh, $table_blog, $uid, $_USER_LOGIN);
+
+        //echo 'New user added.' , PHP_EOL;
+        return 'ok';
+    } else {
+        // echo 'Exists!!!' , PHP_EOL;
+        // echo '-------------------------------------------', PHP_EOL;
+        // foreach ($found_users as $user) {
+        // echo "{$user['user_id']} | {$user['user_login']} | {$user['user_mail']} | {$user['user_profile_name']} | ", PHP_EOL;
+        // }
+        // echo '-------------------------------------------', PHP_EOL;
+        // echo 'Создать такого пользователя нельзя!', PHP_EOL;
+        return 'err_login_mail';
+    }
 }
 
-// ===================
-
-/**
- * Проверяет существование пользователя
- *
- * @param PDO $pdo
- * @param $table_user
- * @param bool $login
- * @param bool $email
- * @return array
- */
 function checkUserExist(PDO $pdo, $table_user, $login = false, $email = false)
 {
     $sql = "
 SELECT user_id, user_login, user_mail, COALESCE(user_profile_name, '----') AS user_profile_name FROM {$table_user} WHERE `user_login` = :user_login OR `user_mail` = :user_mail
     ";
     $data = [
-        'user_login'    =>  $login,
-        'user_mail'     =>  $email
+        'user_login' => $login,
+        'user_mail' => $email
     ];
-    
+
     $sth = $pdo->prepare($sql);
     $sth->execute($data);
     return $sth->fetchAll();
@@ -143,7 +168,7 @@ SELECT user_id, user_login, user_mail, COALESCE(user_profile_name, '----') AS us
 function addBlog(PDO $pdo, $table_blog, $user_owner_id, $login)
 {
     global $dictionary;
-    
+
     $sql = "
 INSERT INTO {$table_blog}
     SET user_owner_id = :user_owner_id,
@@ -156,15 +181,15 @@ INSERT INTO {$table_blog}
         blog_avatar = :blog_avatar
 ";
     $data = [
-        'user_owner_id'     =>  $user_owner_id,
-        'blog_title'        =>  $dictionary['blogs_personal_title'] . ' ' . $login,
-        'blog_description'  =>  $dictionary['blogs_personal_description'],
-        'blog_url'          =>  null,
-        'blog_avatar'       =>  null
+        'user_owner_id' => $user_owner_id,
+        'blog_title' => $dictionary['blogs_personal_title'] . ' ' . $login,
+        'blog_description' => $dictionary['blogs_personal_description'],
+        'blog_url' => null,
+        'blog_avatar' => null
     ];
-    
+
     //echo "Creating blog for `{$login}` (id: {$user_owner_id})" , PHP_EOL;
-    
+
     $sth = $pdo->prepare($sql);
     return $sth->execute($data);
 }
@@ -191,49 +216,59 @@ INSERT INTO {$table_user}
         user_activate = 1,
         user_activate_key = ''
 ";
-    
+
     $data = [
-        'user_login'    =>  $login,
-        'user_password' =>  md5($pass),
-        'user_mail'     =>  $email
+        'user_login' => $login,
+        'user_password' => md5($pass),
+        'user_mail' => $email
     ];
-    
+
     $sth = $pdo->prepare($sql);
     $sth->execute($data);
-    
+
     //echo "Creating user `{$login}`, e-mail: `{$email}`" , PHP_EOL;
-    
+
     return $pdo->lastInsertId();
 }
 
-function checkParam($Param){
-	return !(empty($Param) || $Param==false || is_null($Param) || strlen($Param)<6);
+function checkParam($Param)
+{
+    return !(empty($Param) || $Param == false || is_null($Param) || strlen($Param) < 6);
 }
-if($RespData['stat']=='add') {
-	if(!check_captcha($RespData['captcha'])){
-		$responseCode = 'err_cap';
-		return;
-	}
-	if(!checkParam($RespData['login'])){
-		$responseCode = 'err_login';
-		return;
-	}
-	if(!checkParam($RespData['email'])){
-		$responseCode = 'err_email';
-		return;
-	}
-	if(!checkParam($RespData['pass'])){
-		$responseCode = 'err_pass';
-		return;
-	}
-	
-	$responseCode = create_user($RespData['login'], $RespData['email'], $RespData['pass']);
-	
-	/*if($resp == 1) {
-		echo "ok";
-	} else {
-		echo "error";
-	}*/
-} 
+
+// MAIN
+
+if ($post_data['stat'] == 'add') {
+    /*if (!check_captcha_google($post_data['captcha'])) {
+        $responseCode = 'err_cap';
+        return;
+    }*/
+
+    if (!check_captcha_turnstile()) {
+        $responseCode = 'err_cap';
+        return;
+    }
+
+    if (!checkParam($post_data['login'])) {
+        $responseCode = 'err_login';
+        return;
+    }
+    if (!checkParam($post_data['email'])) {
+        $responseCode = 'err_email';
+        return;
+    }
+    if (!checkParam($post_data['pass'])) {
+        $responseCode = 'err_pass';
+        return;
+    }
+
+    $responseCode = create_user($post_data['login'], $post_data['email'], $post_data['pass']);
+
+    /*if($resp == 1) {
+        echo "ok";
+    } else {
+        echo "error";
+    }*/
+}
 
 echo $responseCode;
